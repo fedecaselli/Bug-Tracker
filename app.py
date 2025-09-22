7f8e21f4513707b52622396b345c6d65a4676f00
Federica Iola Caselli <160833909+fedecas123@users.noreply.github.com>
2025-09-22 15:03:36 +0200

    feat(db): added SQLAlchemy engine, SessionLocal and Base

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from core.db import engine
+from core.models import Base
 
 app = FastAPI(title = "BugTracker")
 
@@ -9,3 +11,7 @@ def health_check():
     return {"status": "healthy"}
 
 app.mount("/static", StaticFiles(directory="web/static"), name="static")
+
+
+#create database tables
+Base.metadata.create_all(bind=engine)
--- core/db.py
+++ core/db.py
@@ -1,10 +1,24 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from config import DATABASE_URL
-
+'''
 #SQLALCCHEMY ORM STYLE
 engine = create_engine(DATABASE_URL)
+Base = declarative_base()
+print("DB ready")
+'''
 
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from config import DATABASE_URL
+
+# Basic connection to SQLite Database
+engine = create_engine(DATABASE_URL,connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-print("DB ready")
+#Function responsible for managing database sessions
+def get_db():
+    db = SessionLocal() #each session is a connection to db
+    try:
+        yield db
+    finally:
+        db.close()
